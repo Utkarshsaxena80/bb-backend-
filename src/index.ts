c3ba9433a9463ca07c512a4f81f6cc28357eb182
Utkarsh saxena <151988496+Utkarshsaxena80@users.noreply.github.com>
2025-08-07 18:48:37 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:"https://bb-frontend-seven.vercel.app/",
+    origin:"https://bb-frontend-seven.vercel.app",
     methods:['GET','POST'],
     credentials:true
 }))
