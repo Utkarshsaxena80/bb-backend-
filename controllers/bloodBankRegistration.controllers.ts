681b6022efe16f7400c9e6552a0991acf7c4c69d
Utkarsh saxena <151988496+Utkarshsaxena80@users.noreply.github.com>
2025-08-09 16:40:28 +0530

    Update bloodBankRegistration.controllers.ts

--- controllers/bloodBankRegistration.controllers.ts
+++ controllers/bloodBankRegistration.controllers.ts
@@ -87,7 +87,7 @@ const bloodR = async (req: Request, res: Response): Promise<void> => {
       res.cookie("authToken", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "none",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
